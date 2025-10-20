0edae651e28a6778f352eaf543cfe83320726e53
Safeer Ahmad Rana <pakistanireacts9@gmail.com>
2025-10-20 23:22:14 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -69,6 +69,9 @@ export default function Home() {
         {/* Scroll to Top Button */}
         <ScrollToTopButton />
 
+        {/* Scroll to Top Button */}
+        <ScrollToTopButton />
+
 
       </div>
     </>
